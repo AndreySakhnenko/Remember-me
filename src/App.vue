12bf67b30dd1866204304ee8de04089e3781ce81
Andrey_Sakhnenko <senya310594@gmail.com>
2022-09-10 23:30:40 +0300

    add games

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app_container">
+    <h1 class="app_title">Remember Me</h1>
+    <p class="app_text">Запам'ятай та відкрий всі зелені клітинки</p>
+  </div>
+  <BoardGame />
+</template>
+<script>
+import BoardGame from "./components/BoardGame.vue";
+export default {
+  components: {
+    BoardGame,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  text-align: center;
+}
+.app {
+  &_container {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    margin-top: 40px;
+  }
+}
+</style>
